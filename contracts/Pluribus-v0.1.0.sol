a0306cfcbf4df2138130978a2d147bcfa4059e09
Mills McIlroy <37815163+millsmcilroy@users.noreply.github.com>
2023-06-14 22:37:16 -0600

    WIP - spiking which ERCs to use

---
+++ contracts/Pluribus-v0.1.0.sol
@@ -0,0 +1,13 @@
+// contracts/PluribusVault-v0.1.0.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
+
+contract Pluribus is ERC4626 {
+
+}
+
+contract PluribusVault is ERC4626 {
+
+}
